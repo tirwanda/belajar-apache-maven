d48900f688436af975e35faa2de33b578a9ef2d5
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-05-29 23:22:53 +0700

    Setup Apache Maven

---
+++ src/main/java/maven/App.java
@@ -0,0 +1,19 @@
+package maven;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+        Person person = new Person("Edho Dwi Tirwanda");
+
+        String json = gson.toJson(person);
+        System.out.println(json);
+    }
+
+}
